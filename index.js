0301e470a3980fa12c1ee28d873403d846a17ad5
tuong.lau16072002@hcmut.edu.vn <tuong.lau16072002@hcmut.edu.vn>
2021-08-28 18:44:38 +0700

    express.static

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
-
-
 app.set('view engine', 'pug')
-app.get('/',(req,res)=>{
-    console.log(users)
-})
-app.get('/user', (req, res) => {
-
-    console.log(users)
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
 app.listen(port, () => console.log(`Server conecting to ${port}`))
